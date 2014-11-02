fbf1082d6d7392f2bd00075321b8afc2e136e6fb
airywenter <idanan@Lenovo.(none)>
2014-11-02 20:34:28 +0800

    deal with data

--- transform.py
+++ transform.py
@@ -1,15 +1,29 @@
 #-*-encoding:utf-8-*-
-import pandas as pd
-import numpy as np
 import xlwt
+import re
+import pandas as pd
 
 PATH='/home/idanan/jiayuan/user_infos.txt'
 header=['id','gender','age','where','height','edu',\
-		'marriage','salary','nation','job',\
-		'car','house','look','body','face',\
+		'marriage','marr2','salary','nation','job',\
+		'car','house','look','look2','body','face',\
 		'hair','weight','place','smoke','drink',\
 		'personality','child','parent']
-MAPPY={'愿意':'1','不愿意':'-1','视情况而定':'0','暂未购车':'-1','已经购车':'1','已购住房':'1','暂未购房':'0','需要时购置':'0','与父母同住':'0','住亲朋家':'0','住单位家':'0','住单位房':0,'与人合租':'0','独自租房':'0','中专或相当学历':'1','大专':'2','本科':'3','双学士':'4','硕士':'5','博士':'6','博士后':'7','未婚':'0,0','离异,无小孩':'1,0','离异,有小孩归自己':'1,1','离异':'1,1','离异,有小孩归对方':'1,1','丧偶,无小孩':'1,0','丧偶,有小孩归自己':'1,1','丧偶,有小孩归对方':'1,1','苗条':'-1','高挑':'-1','匀称':'0','丰满':'1','健壮':'2','魁梧':'2','方脸型':'1','国字脸型':'1','菱形脸型':'1','三角脸型':'1','圆脸型':'0','鸭蛋脸型':'0','瓜子脸型':'0','不吸，很反感吸烟':'3','不吸，但不反感':'1','社交时偶尔吸':'-1','每周吸几次':'-2','每天都吸':'-4','有烟瘾':'-5','社交需要时喝':'0','有兴致时喝':'-1','每天都离不开酒':'-4','不喝':'1','光头':'0','很短':'1','短发':'2','中等长度':'3','卷曲长发':'5','顺直长发':'5','淡雅如菊':'1,0','娇小依人':'0,0','眉清目秀':'1,0','明眸善睐':'0,1','成熟魅力':'1,1','青春活泼':'0,0','秀外慧中':'0,1','雍容华贵':'1,1','未填':'NA'}
+MAPPY={'愿意':'1','不愿意':'-1','视情况而定':'0','暂未购车':'-1','已经购车':'1',\
+		'已购住房':'1','暂未购房':'0','需要时购置':'0','与父母同住':'0','住亲朋家':'0','住单位家':'0',\
+		'住单位房':0,'与人合租':'0','独自租房':'0',\
+		'中专或相当学历':'1','大专':'2','本科':'3','双学士':'4','硕士':'5','博士':'6','博士后':'7',\
+		'未婚':'0,0','离异,无小孩':'1,0','离异,有小孩归自己':'1,1','离异':'1,1','离异,有小孩归对方':'1,1',\
+		'丧偶,无小孩':'1,0','丧偶,有小孩归自己':'1,1','丧偶,有小孩归对方':'1,1',\
+		'苗条':'-1','高挑':'-1','匀称':'0','丰满':'1','健壮':'2','魁梧':'2',\
+		'方脸型':'1','国字脸型':'1','菱形脸型':'1','三角脸型':'1','圆脸型':'0','鸭蛋脸型':'0','瓜子脸型':'0',\
+		'不吸，很反感吸烟':'3','不吸，但不反感':'1','社交时偶尔吸':'-1','每周吸几次':'-2','每天都吸':'-4','有烟瘾':'-5',\
+		'社交需要时喝':'0','有兴致时喝':'-1','每天都离不开酒':'-4','不喝':'1',\
+		'光头':'0','很短':'1','短发':'2','中等长度':'3','卷曲长发':'5','顺直长发':'5',\
+		'淡雅如菊':'1,0','娇小依人':'0,0','眉清目秀':'1,0','明眸善睐':'0,1','成熟魅力':'1,1','青春活泼':'0,0',\
+		'秀外慧中':'0,1','雍容华贵':'1,1','未填':'NA','10000～20000元':'2','20000元以上':'3','2000元以下':'-1',\
+		'2000～5000元':'0','5000～10000元':'1','女':'M'}
+
 
 def loadDict(fpath):
 	mappy={}
@@ -49,10 +63,6 @@ def transform(data,mappy):
 			return mappy[x]
 		else:
 			return x
-#	def departInfo(info,filters):
-#		info1=[info[i] for i in filters]
-#		info2=[info[i] for i  in range(23) if i not in FILTERS]
-#		return [info1,info2]
 	new_data=[]
 	for info in data:
 		new_info=map(mapping,info)
@@ -65,6 +75,29 @@ def saveData(data,path):
 		for line in data:
 			s=[str(i) for i in line]
 			f.write('|'.join(s)+'\n')
+
+
+def nummeric(data,columns):
+	def search(x):
+		result=re.search('(\d+)',x)
+		if result:
+			return result.group()
+		else:
+			return 0
+
+	for i,row in enumerate(data):
+		for j in columns:
+			data[i][j]=search(row[j])
+	return data
+
+def addColumn(data,columns):
+	for i,row in enumerate(data):
+		for j in columns:
+			if row[j]=='NA':
+				data[i][j]='NA|NA'
+			else:
+				data[i][j]=re.sub(',','|',row[j])
+	return data
 
 
 def quantile(array,quantiles):
@@ -80,18 +113,22 @@ def quantile(array,quantiles):
 def save_excel(fpath,name,lists):
 	workbook=xlwt.Workbook()
         sheet=workbook.add_sheet(name)
-	height=len(lists)
-	width=len(lists[1])
-	for i in range(height):
-		li=lists[i]
-		for j in range(width):
-			string=li[j].decode('utf-8')
-			sheet.write(i,j,string)
+	for i,row in enumerate(lists): 
+		for j,e in enumerate(row):
+			element=e.decode('utf-8')
+			sheet.write(i,j,element)
 	workbook.save(fpath+'/'+name+'.xls')
 
-infos=loadData('/home/idanan/jiayuan/parsed_female_info.txt')
-save_excel('/home/idanan/jiayuan','parsed_female',infos)
+def apply(fpath,dpath,func,args):
+	data=loadData(fpath)
+	data=func(data,args)
+	saveData(data,dpath)
 
-	
+#data=loadData('/home/idanan/jiayuan/user_info.txt')
+#male,female=departData(data)
+#female=nummeric(female,[2,4,16])
+#female=transform(female,MAPPY)
+#saveData(female,'/home/idanan/jiayuan/transed_female.txt')
 
-						
+#data=loadData('/home/idanan/jiayuan/result_female.txt')
+#save_excel('/home/idanan/jiayuan','result_female',data)
