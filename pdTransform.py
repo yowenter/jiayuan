b60aa4ec369d98704282c84892a5a82c737022f8
airywenter <idanan@Lenovo.(none)>
2014-11-02 23:02:44 +0800

    suck pandas

--- pdTransform.py
+++ pdTransform.py
@@ -6,11 +6,15 @@ Email:airywent@gmail.com
 '''
 import pandas as pd
 import numpy as np
+import re
+
 header=['id','gender','age','where','height','edu',\
 		'marriage','salary','nation','job',\
                 'car','house','look','body','face',\
                 'hair','weight','place','smoke','drink',\
                 'personality','child','parent']
+
+
 
 #The string in text is decoded as utf-8,while the string in shell need to be decode to utf-8
 
@@ -26,7 +30,9 @@ mapping={'愿意':'1','不愿意':'-1','视情况而定':'0','暂未购车':'-1'
 	'社交需要时喝':'0','有兴致时喝':'-1','每天都离不开酒':'-4','不喝':'1',\
 	 '光头':'0','很短':'1','短发':'2','中等长度':'3','卷曲长发':'5','顺直长发':'5',\
 	 '淡雅如菊':'1,0','娇小依人':'0,0','眉清目秀':'1,0','明眸善睐':'0,1','成熟魅力':'1,1','青春活泼':'0,0',\
-	 '秀外慧中':'0,1','雍容华贵':'1,1','未填':'NA'}
+	 '秀外慧中':'0,1','雍容华贵':'1,1',
+	 '10000～20000元':'2','20000元以上':'3','2000元以下':'-1','2000～5000元':0,'5000～10000元':1}
+
 
 
 def loadData(fpath):
@@ -38,23 +44,60 @@ def departData(data):
 	male=data[data['gender']=='男']
 	return female,male
 
+#convert the values of columns from continus to descrete,and filter the outliers.
 
+def nummeric(data,columns):
+	def search(x):
+		result=re.search('(\d+)',str(x))
+		if result:
+			return result.group()
+		else:
+			return 0
+	for col in columns:
+		data[col]=data[col].map(search).astype(int)
+	return data
+
+def quantize(data,columns):
+	for col in columns:
+		values=data[col].describe()
+		rates=[values['min'],values['25%'],values['50%'],values['75%'],values['max']]
+		#col_data=data[col] ,slice can't be as column in data !
+		#data[col]=col_data[(np.abs(col_data-mean)<3*std)]
+		col_data=pd.cut(data[col],rates,labels=[-1,0,1,2])
+		data[col]=col_data
+		return data
+
+def calbmi(data):
+	def bmi(w,h):
+		if w!=0 and h!=0:
+			return float(w)/(h*h)
+		else:
+			return 0
+	f=lambda x:bmi(x['weight'],x['height'])
+	data['bmi']=data.apply(f,axis=1)
+	return data
+
+
+#map data to transform from string to nummeric
 def transform(data,mapping):
 	def xmap(x):
 		if x in mapping:
 			return mapping[x]
 		else:
-			return x
+			return 0
 	for col in header:
-		data[col]=data[col].map(xmap)
+		data.loc[:,col].map(xmap)
 	return data
 
 
 def saveData(data,fpath):
-	data.to_csv(fpath,setp='::',index=False,header=False)
-
-
+	data.to_csv(fpath,sep='|',index=False,header=False)
+pd.set_option('mode.chained_assignment',None)
 data=loadData('/home/idanan/jiayuan/user_info.txt')
 female,male=departData(data)
-female=transform(female,mapping)
-saveData(female,'/home/idanan/jiayuan/transed_female.txt')
+female=nummeric(female,['age','height','weight'])
+saveData(female,'/home/idanan/jiayuan/pd_female.txt')
+#ifemale=calbmi(female)
+
+#female=quantize(female,['height','weight'])
+#saveData(transform(female,mapping),'/home/idanan/jiayuan/transed_female.txt')
