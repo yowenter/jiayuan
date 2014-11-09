b4ead79941c70ef174c145ef5d19a93626c3b3e2
airywenter <airywenter@foxmail.com>
2014-11-09 20:02:31 +0800

    asd

--- marryModel.py
+++ marryModel.py
@@ -1,12 +1,12 @@
 #-*-encoding:utf-8 -*-
-from time import time
 import numpy as np
 import pandas as pd
-from pdTransform import loadData,saveData
+from pdTransform import saveData
 from sklearn import metrics
 from sklearn.cluster import KMeans
-from sklearn.decomposition import PCA
+from sklearn.decomposition import PCA,FactorAnalysis
 from sklearn.preprocessing import scale
+import re
 
 def matchDict(fpath):
 	matches={}
@@ -24,40 +24,41 @@ def concatData(female,male,matches):
 	data=pd.merge(male,female,on='id')
 	return data
 
+def factors(data,components,method='pca'):
+	if method=='pca':
+		pca=PCA(n_components=components).fit(data)
+	if method=='factor':
+		pca=FactorAnalysis(n_components=components,svd_method='randomized').fit(data)	
+	return pca
 
+def scaleData(data,filters):
+	new_data=data.drop(labels=filters,axis=1)
+	new_data=new_data.fillna(0).astype(float).values
+	new_data=scale(new_data)
+	return new_data
 
-
-
-
-def kmeansModel(data,filters):
-	def bench_k_means(estimator,data,filters):
+def kmeansModel(data,n_digits,initValue='k-means++'):
+	def bench_k_means(estimator,data):
 		estimator.fit(data)
 		return estimator.labels_
-
-	new_data=data.drop(labels=filters,axis=1)
-	new_data=new_data.fillna(0).astype(float)
-	new_data=scale(new_data.values)
-	n_digits=
-	pca=PCA(n_components=15).fit(new_data)
-	print pca.explained_variance_ratio_
-	print sum(pca.explained_variance_ratio_)
-	results=bench_k_means(KMeans(init=pca.components_,n_clusters=n_digits,n_init=1),new_data,filters)
-	data['class']=results
-	return data
+	results=bench_k_means(KMeans(init=initValue,n_clusters=n_digits,n_init=10),data)
+	return results
 
 
 def decisionModel(data):
-
-	
-
+	pass
 
 
-header='id|gender|age|height|edu|salary|nation|car|house|body|face|hair|\
-smoke|drink|child|parent|bmi|where2|where0|where1|\
-marriage0|marriage1|look0|look1'.split('|')
-data=pd.read_csv('/home/idanan/jiayuan/code/pd_female1.txt',names=header,sep='|')
-data=kmeansModel(data,['id','gender'])
-saveData(data,'/home/idanan/jiayuan/code/cluster_female.txt')
-
-
+def main():
+	header=re.sub(' |\t','','id|gender|age|height|edu|salary|nation|car|house|body|face|hair|\
+	smoke|drink|child|parent|bmi|where2|where0|where1|\
+	marriage0|marriage1|look0|look1').split('|')
+	data=pd.read_csv('/home/idanan/jiayuan/code/pd_female1.txt',names=header,sep='|')
+	new_data=scaleData(data,['id','gender'])
+	pca=factors(new_data,18)
+	print 'PCA explained variance:', sum(pca.explained_variance_ratio_)
+	new_data=pca.transform(new_data)
+	results=kmeansModel(new_data,6)
+	data['cluster']=results
+	saveData(data,'/home/idanan/jiayuan/code/cluster_female2.txt')
 
